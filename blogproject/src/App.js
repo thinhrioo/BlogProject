9b705517a734e396bb6dec22e8a6f3d67bc24106
thinhrio <ducthinh.0203.a2@gmail.com>
2024-07-05 16:33:36 +0700

    update components

--- blogproject/src/App.js
+++ blogproject/src/App.js
@@ -1,10 +1,28 @@
 
+import Topbar from "./components/topbar/Topbar";
+import Homepage from "./pages/homepage/Homepage";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import { BrowserRouter , Routes, Route } from "react-router-dom";
 
 function App() {
+  const currentUser = true;
   return (
-    <div>
-      <h1>Hello this is app components</h1>
-    </div>
+    <BrowserRouter>
+    <Topbar />
+        <Routes>
+          <Route path='/' element={<Homepage/>}></Route>
+          <Route path='/register' element={currentUser ? <Homepage /> : <Register />}></Route>
+          <Route path="/login" element={currentUser ? <Homepage /> : <Login />}></Route>
+          <Route path="/post/:id" element={<Single />}></Route>
+          <Route path="/write" element={currentUser ? <Write /> : <Login />}></Route>
+          <Route path="/settings" element={currentUser ? <Settings /> : <Login />}></Route>
+         
+        </Routes>
+    </BrowserRouter>
   );
 }
 
